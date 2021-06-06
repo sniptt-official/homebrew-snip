0e0adc150ddc00d564d9cce672b5584c0064a4ff
Roger <roger@sniptt.com>
2021-06-06 12:15:24 +0100

    fix(formula): fix homebrew formula

--- Formula/snip.rb
+++ Formula/snip.rb
@@ -1,9 +1,9 @@
 class Snip < Formula
   desc "🔐 The Secret Manager that developers love!"
   homepage "https://github.com/sniptt-official/cli"
-  url "https://bin.sniptt.com/macos/v0.0.38/snip.tar.gz"
-  sha256 "cce8293125b90d3e2b148565e3bfc55f067b3bdbff8b60cd7c4fa3d563472dfd"
-  version "0.0.38"
+  url "https://github.com/sniptt-official/snip-cli/releases/download/v0.0.39/snip-macos-x64.tar.gz"
+  sha256 "1dbf9ffab04eb96641c82f335b2eb5b9b51f7c3af45f25772f039ec7654bf737"
+  version "0.0.39"
 
   def install
     # bin.install "sniptt-macos" => "snip"
