6045b369d346ad7514e2607f18a438d21b8d6730
Slavo Vojacek <public@slavovojacek.com>
2021-07-24 17:47:58 +0100

    chore(formula): update formula

--- Formula/snip.rb
+++ Formula/snip.rb
@@ -1,12 +1,11 @@
 class Snip < Formula
-  desc "🔐 The Secret Manager that developers love!"
-  homepage "https://github.com/sniptt-official/snip-cli"
-  url "https://github.com/sniptt-official/snip-cli/releases/download/v0.0.50/snip-macos-x64.tar.gz"
-  sha256 "2e42a770ce571a6557dd31274e1771806d3b1c9a9fc245a22c5ca8b04cb55134"
-  version "0.0.50"
+  desc "🔐 A simple developer-friendly CLI for managing secrets"
+  homepage "https://github.com/sniptt-official/snip"
+  url "https://github.com/sniptt-official/snip/releases/download/v0.0.51/snip-macos-x64.tar.gz"
+  sha256 "0474d38239bcc089ba459fafb5d0cd19000c7e89429f82f498ccb37a2886319e"
+  version "0.0.51"
 
   def install
-    # bin.install "sniptt-macos" => "snip"
     bin.install "snip"
   end
 end
